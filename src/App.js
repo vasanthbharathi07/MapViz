c41026c364ba4e4e8297b380a7f1cb9e6c129c49
vasanthbharathi07 <vasanthbharathi07@gmail.com>
2024-10-02 23:32:48 +0530

    boiler plate code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MapComponent from './component/MapComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <MapComponent/>
+    </div>
+  );
+}
+
+export default App;
