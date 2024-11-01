ede932f90378c216b1791aed0bf6bf0089f93848
vasanthbharathi07 <vasanthbharathi07@gmail.com>
2024-11-01 14:27:22 +0530

    Added API Service to fetch JSON data from local drive

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { WEATHER_API_BASE_URL } from "../constants/url_constants";
+import { parseElectionData } from "../utils/utilities";
 
 export function getWeatherForGivenCity(cityName) {
   let apiUrl = WEATHER_API_BASE_URL + "/" + cityName;
@@ -12,4 +13,39 @@ export function getWeatherForGivenCity(cityName) {
       console.log("Error while hitting Weather API = ", error);
       throw error;
     });
+}
+
+
+export async function fetchElectionResultsData(country) {
+  try {
+    if (country === "United States Of America") {
+      const response = await fetch("/resources/US_elections_2020_csv.csv"); // Assuming it's in the public/resources folder
+      if (!response.ok) {
+        throw new Error(`Failed to fetch GeoJSON: ${response.statusText}`);
+      }
+      const data = await response.text();
+      const row_data = data?.split('\n');
+      const electionDataList = row_data.map(parseElectionData);
+      return electionDataList;
+      //setElectionResultsData(electionDataList); // Set GeoJSON data in state
+    }
+  } catch (error) {
+    console.error("Error fetching GeoJSON data:", error);
+  }
+}
+
+export async function fetchGeoJsonData(country) {
+  try {
+    if (country === "United States Of America") {
+      const response = await fetch("/resources/us-state-boundaries.geojson"); // Assuming it's in the public/resources folder
+      if (!response.ok) {
+        throw new Error(`Failed to fetch GeoJSON: ${response.statusText}`);
+      }
+      const data = await response.json();
+      //setGeoJsonData(data); // Set GeoJSON data in state
+      return data;
+    }
+  } catch (error) {
+    console.error("Error fetching GeoJSON data:", error);
+  }
 }
