6f37672f5660c7dd9f11083049499b515b457830
vasanthbharathi07 <vasanthbharathi07@gmail.com>
2024-11-04 07:29:59 +0530

    Added appconstants file to reuse them across components

---
+++ src/constants/AppConstants.js
@@ -0,0 +1,16 @@
+export const COUNTRY_NAME_US = 'United States Of America';
+export const COLOR_GREEN = 'Green';
+export const YEARS_LIST = [[2012, 2016,2020]];
+export const DEFAULT_YEAR_VALUE = [2020];
+export const OPACITY_VALUE = 0.7;
+export const WEIGHT_VALUE = 2;
+
+export const CSV_2012_DATA_FILE_NAME = 'US_elections_2012_csv.csv';
+export const CSV_2016_DATA_FILE_NAME = 'US_elections_2016_csv.csv';
+export const CSV_2020_DATA_FILE_NAME = 'US_elections_2020_csv.csv';
+export const YEAR_2012 = '2012';
+export const YEAR_2016 = '2016';
+export const YEAR_2020 = '2020';
+
+export const COUNTRY_US_LAT = 39.8283;
+export const COUNTRY_US_LONG = -98.5795;
